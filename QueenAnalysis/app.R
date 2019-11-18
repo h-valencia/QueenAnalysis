966d53a3bb335abe3c4f78dccc395f7bcf44f0e2
Hannah <hvalencia@college.harvard.edu>
2019-11-17 21:46:39 -0500

    Filtered out duplicate songs

--- QueenAnalysis/app.R
+++ QueenAnalysis/app.R
@@ -11,36 +11,17 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 ui <- navbarPage("Navbar!",
-                 tabPanel("Danceability",
+                 tabPanel("Audio Features",
                           sidebarLayout(
                               sidebarPanel(
-                                  selectInput("select", h3("Album"), 
-                                                       choices = list("Queen" = 1, 
-                                                                      "Queen II" = 2, 
-                                                                      "Sheer Heart Attack" = 3, 
-                                                                      "A Night At The Opera" = 4, 
-                                                                      "A Day At The Races" = 5,
-                                                                      "News Of The World" = 6,
-                                                                      "Jazz" = 7,
-                                                                      "The Game" = 8,
-                                                                      "Flash Gordon" = 9,
-                                                                      "Hot Space" = 10,
-                                                                      "The Works" = 11,
-                                                                      "A Kind Of Magic" = 12,
-                                                                      "The Miracle" = 13, 
-                                                                      "Innuendo" = 14,
-                                                                      "Made In Heaven" = 15), selected = 1))
-                                  
-                              
-                              ,
-                              mainPanel(
-                                  plotOutput("distPlot")
-                              ))    
-                          ),
-                 tabPanel("Energy",
-                          sidebarLayout(
-                              sidebarPanel(
-                                  selectInput("select", h3("Album"), 
+                                  helpText("Choose one of Queen's 15 studio albums to obtain a graph with each song's liveness"),
+                                  selectInput("feature", h3("Feature"),
+                                              choices = list("Danceability" = 1,
+                                                             "Energy" = 2,
+                                                             "Liveness" = 3,
+                                                             "Speechiness" = 4,
+                                                             "Accousticness" = 5)),
+                                  selectInput("album", h3("Album"), 
                                               choices = list("Queen" = 1, 
                                                              "Queen II" = 2, 
                                                              "Sheer Heart Attack" = 3, 
@@ -55,9 +36,14 @@ ui <- navbarPage("Navbar!",
                                                              "A Kind Of Magic" = 12,
                                                              "The Miracle" = 13, 
                                                              "Innuendo" = 14,
-                                                             "Made In Heaven" = 15), selected = 1))
-                              
-                              
+                                                             "Made In Heaven" = 15), selected = 1),
+                                  p(strong("Danceability:"), "describes how suitable a track is for dancing based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity. A value of 0.0 is least danceable and 1.0 is most danceable."),
+                                  p(strong("Energy:"), "a measure from 0.0 to 1.0 and represents a perceptual measure of intensity and activity. Typically, energetic tracks feel fast, loud, and noisy. Perceptual features contributing to this attribute include dynamic range, perceived loudness, timbre, onset rate, and general entropy."),
+                                  p(strong("Liveness:"), "detects the presence of an audience in the recording. Higher liveness values represent an increased probability that the track was performed live. A value above 0.8 provides strong likelihood that the track is live."),
+                                  p(strong("Speechiness:"), "detects the presence of spoken words in a track. The more exclusively speech-like the recording, the closer to 1.0 the attribute value. Values above 0.66 describe tracks that are probably made entirely of spoken words. Values between 0.33 and 0.66 describe tracks that may contain both music and speech. Values below 0.33 most likely represent music and other non-speech-like tracks."),
+                                  p(strong("Accousticness:"), "a confidence measure from 0.0 to 1.0 of whether the track is acoustic. 1.0 represents high confidence the track is acoustic.")
+                                  
+                                  )
                               ,
                               mainPanel(
                                   plotOutput("distPlot")
